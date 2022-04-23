b6c45d2fd8c21809379d22c05985332c4674c8ca
Miguel Armas <miguel.a.armas@gmail.com>
2022-04-23 11:59:59 +0100

    Cambios realizados en el método formatText y agregado de la función findBreakPoint para identificar los espacios en blanco dentro del texto

--- src/main/java/Editor.java
+++ src/main/java/Editor.java
@@ -3,21 +3,41 @@ public class Editor {
 
     public StringBuilder formatText(String strOriginal, int lineWidth) {
         StringBuilder strResult = new StringBuilder();
+        int changePosition = 0;
 
         for (int actualPosition = 0; actualPosition < strOriginal.length(); actualPosition += lineWidth) {
+
             if (actualPosition > 0) {
                 strResult.append("\n");
             }
 
-            if (actualPosition + lineWidth > strOriginal.length()) {
+            if (changePosition != 0) {
+                actualPosition -= changePosition;
+                changePosition = 0;
+            }
+
+            if (actualPosition + lineWidth > strOriginal.length()) {  // <-- End of text
                 strResult.append(strOriginal.substring(actualPosition) );
             } else {
-                strResult.append(strOriginal.substring(actualPosition, actualPosition+lineWidth) );
+
+                int breakPos = findBreakPoint(strOriginal, actualPosition, lineWidth);
+                strResult.append(strOriginal.substring(actualPosition, breakPos) );
+                changePosition = lineWidth - breakPos + actualPosition;
+
             }
         }
 
         return strResult;
 
+    }
+
+    public int findBreakPoint(String strOriginal, int actualPosition, int lineWidth) {
+        for (int i = lineWidth-2; i >=0; i--){
+            if(strOriginal.substring(actualPosition+i, actualPosition+i+1).equals(" ")) {
+                return actualPosition + i + 1;
+            }
+        }
+        return actualPosition+lineWidth;
     }
 
 
--- src/test/java/EditorShould.java
+++ src/test/java/EditorShould.java
@@ -40,17 +40,34 @@ public class EditorShould {
 
     @Test
     void lineBreaksOnSpacesIfExists() {
-        originalText = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500.";
+        originalText = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas \"Letraset\", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.";
         lineWidth = 40;
 
         formattedTextExpected = new StringBuilder();
-        formattedTextExpected.append("Lorem Ipsum es simplemente el texto de").append("\n");
-        formattedTextExpected.append("relleno de las imprentas y archivos de").append("\n");
-        formattedTextExpected.append("texto. Lorem Ipsum ha sido el texto de").append("\n");
-        formattedTextExpected.append("relleno estándar de las industrias").append("\n");
-        formattedTextExpected.append("desde el año 1500.");
-
+        formattedTextExpected.append("Lorem Ipsum es simplemente el texto de ").append("\n");
+        formattedTextExpected.append("relleno de las imprentas y archivos de ").append("\n");
+        formattedTextExpected.append("texto. Lorem Ipsum ha sido el texto de ").append("\n");
+        formattedTextExpected.append("relleno estándar de las industrias ").append("\n");
+        formattedTextExpected.append("desde el año 1500, cuando un impresor ").append("\n");
+        formattedTextExpected.append("(N. del T. persona que se dedica a la ").append("\n");
+        formattedTextExpected.append("imprenta) desconocido usó una galería ").append("\n");
+        formattedTextExpected.append("de textos y los mezcló de tal manera ").append("\n");
+        formattedTextExpected.append("que logró hacer un libro de textos ").append("\n");
+        formattedTextExpected.append("especimen. No sólo sobrevivió 500 ").append("\n");
+        formattedTextExpected.append("años, sino que tambien ingresó como ").append("\n");
+        formattedTextExpected.append("texto de relleno en documentos ").append("\n");
+        formattedTextExpected.append("electrónicos, quedando esencialmente ").append("\n");
+        formattedTextExpected.append("igual al original. Fue popularizado en ").append("\n");
+        formattedTextExpected.append("los 60s con la creación de las hojas ").append("\n");
+        formattedTextExpected.append("\"Letraset\", las cuales contenian ").append("\n");
+        formattedTextExpected.append("pasajes de Lorem Ipsum, y más ").append("\n");
+        formattedTextExpected.append("recientemente con software de ").append("\n");
+        formattedTextExpected.append("autoedición, como por ejemplo Aldus ").append("\n");
+        formattedTextExpected.append("PageMaker, el cual incluye versiones ").append("\n");
+        formattedTextExpected.append("de Lorem Ipsum.");
         StringBuilder strAnswer = txtEditor.formatText(originalText, lineWidth);
+
+        System.out.println(strAnswer.toString());
         assertEquals(formattedTextExpected.toString(), strAnswer.toString());
     }
 
